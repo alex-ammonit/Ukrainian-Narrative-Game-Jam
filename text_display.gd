2084ab51f600ef61b0f7a67ceadd28f5055feb18
alex-ammonit <alex.anisimov.aa@gmail.com>
2025-03-18 17:56:27 +0200

    new file:   .gitignore 	new file:   a.png 	new file:   a.png.import 	new file:   b.png 	new file:   b2.png 	new file:   c.png 	new file:   clockhand.gd 	new file:   icon.png 	new file:   icon.svg 	new file:   main_scene.tscn 	new file:   main_scene.tscn4835585279.tmp 	new file:   main_scene.tscn4842350056.tmp 	new file:   polygon_spawner.gd 	new file:   polygon_spawner.gd.uid 	new file:   project.godot 	new file:   selected_button.gd 	new file:   selected_button.gd.uid 	new file:   text_display.gd 	new file:   text_display.gd.uid 	new file:   what_selected.gd 	new file:   what_selected.gd.uid

---
+++ text_display.gd
@@ -0,0 +1,89 @@
+extends RichTextLabel
+
+@export_multiline var t:String
+@export var color_dict:Dictionary[String, Color]
+
+func parse(string:String):
+	print(string)
+	var dictar=[]
+	var in_brackets=false
+	var p_t:String=""
+	for i in len(string)+1:
+		var shall_parse=false
+		#var s=string[i]
+		var s=''
+		#print(s, in_brackets)
+		if (i==len(string)):
+			shall_parse=true
+		else:
+			s=string[i]
+		if (s=='<') or (s=='>'):
+			shall_parse=true
+		if (shall_parse==false):
+			p_t=p_t+s
+		else:
+			if (p_t==""):
+				pass
+			elif (in_brackets==false):
+				dictar.append({"type":"text", "text":p_t})
+				#in_brackets=true
+			elif(in_brackets==true):
+				dictar.append({"type":"control", "data":p_t})
+			p_t=""
+			if (s=='<'):
+				in_brackets=true
+			if (s=='>'):
+				in_brackets=false
+	'''var xml_parser=XMLParser.new()
+	xml_parser.open_buffer(string.to_utf8_buffer())
+	while xml_parser.read()!=ERR_FILE_EOF:
+		#print(xml_parser.get_node_type())
+		match xml_parser.get_node_type():
+			XMLParser.NODE_ELEMENT:
+				print(xml_parser.get_node_name())
+				dictar.append({"type":"node", "name":xml_parser.get_node_name() })
+			XMLParser.NODE_TEXT:
+				print(xml_parser.get_node_data())
+				dictar.append({"type":"text", "text":xml_parser.get_node_data()})'''
+	#for s in string:
+	#	print(s)
+	return dictar
+	pass
+
+func _process(delta):
+	print(parse(t))
+	var elements=parse(t)
+	var new_text=""
+	var activeColor="none"
+	for el in elements:
+		if el["type"]=="control":
+			if (el["data"]=="none"):
+				activeColor="none"
+			if (color_dict.has(el["data"])):
+				new_text=new_text+"[color=#"+color_dict[el["data"]].to_html()+"]"
+				activeColor=el["data"]
+		if el["type"]=="text":
+			if (activeColor!="none" and color_dict[activeColor]==WhatSelected.color):
+				new_text=new_text+el["text"]
+			elif (activeColor=="none"):
+				new_text=new_text+el["text"]
+			else:
+				new_text=new_text+( String("#") )
+			#if el["data"]=="red":
+	text=new_text
+	#pass
+	'''var xmlParser=XMLParser.new()
+	xmlParser.open_buffer( t.to_utf8_buffer() )
+	#print(bytes_to_var(var_to_bytes(t)))
+	while xmlParser.read()!= ERR_FILE_EOF:
+		print("AAA")
+		print(xmlParser.get_node_type())
+		#print(xmlParser.get_node_data())
+		if xmlParser.get_node_type()==XMLParser.NODE_ELEMENT:
+			print("BBB")
+			var node_name=xmlParser.get_node_name()
+			var attribute_dict={}
+			for idx in xmlParser.get_attribute_count():
+				attribute_dict[xmlParser.get_attribute_name(idx)]=xmlParser.get_attribute_value(idx)
+			print(attribute_dict)
+			#print(node_name)'''
---
+++ what_selected.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var selected_id:int=0;
+var color: Color
+var max_freq=1
+var min_freq=-1.5
+@export var freq:float=0:
+	set(value):
+		freq=clamp(value, min_freq, max_freq)
+		#freq=value
+	get:
+		return freq
+
+#func _process(delta):
+	#print(selected_id)
