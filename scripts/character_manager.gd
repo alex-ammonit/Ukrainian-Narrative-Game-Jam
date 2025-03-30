caaa0d77d69b210e08cc4847f8c36dcc7402009f
alex-ammonit <alex.anisimov.aa@gmail.com>
2025-03-30 15:44:31 +0300

    modified:   dialogue/test_text.txt 	new file:   models/char_animations_separate_test.zip 	new file:   models/char_animations_separate_test/MC_animation_separate_test.gltf 	new file:   models/char_animations_separate_test/MC_animation_separate_test.gltf.import 	new file:   models/char_animations_separate_test/MC_animation_separate_test_T_MC_body.png 	new file:   models/char_animations_separate_test/MC_animation_separate_test_T_MC_body.png.import 	new file:   models/char_animations_separate_test/MC_animation_separate_test_T_MC_face_neutral.png 	new file:   models/char_animations_separate_test/MC_animation_separate_test_T_MC_face_neutral.png.import 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test.gltf 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test.gltf.import 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test_T_Nelya_body.png 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test_T_Nelya_body.png.import 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test_T_Nelya_face_neutral.png 	new file:   models/char_animations_separate_test/Nelya_animation_separate_test_T_Nelya_face_neutral.png.import 	new file:   models/char_animations_separate_test/scene_cafe_separate_test.gltf 	new file:   models/char_animations_separate_test/scene_cafe_separate_test.gltf.import 	new file:   models/char_animations_separate_test/scene_cafe_separate_test_t_dark_grey_128x128.png 	new file:   models/char_animations_separate_test/scene_cafe_separate_test_t_dark_grey_128x128.png.import 	new file:   models/char_animations_separate_test/scene_cafe_separate_test_t_grey_128x128.png 	new file:   models/char_animations_separate_test/scene_cafe_separate_test_t_grey_128x128.png.import 	modified:   scripts/character_manager.gd 	modified:   scenes/main_scene.tscn

--- scripts/character_manager.gd
+++ scripts/character_manager.gd
@@ -1,14 +1,26 @@
 extends Node
 
 signal send_expression(new_expression)
+@export var characters:Dictionary[String, Node]
 
 func expression_changed(new_expression):
-	send_expression.emit(new_expression)
-	if (new_expression=="romana:sit_appear"):
+	#send_expression.emit(new_expression)
+	'''if (new_expression=="romana:sit_appear"):
 		$romana_sit.visible=true
 	elif (new_expression=="romana:clone_appear"):
 		$romana_clone.visible=true
 	if (new_expression=="romana:sit_disappear"):
 		$romana_sit.visible=false
 	elif (new_expression=="romana:clone_disappear"):
-		$romana_clone.visible=false
+		$romana_clone.visible=false'''
+	var colon=new_expression.find(':')
+	if colon==-1:
+		pass
+	else:
+		var pre_colon=new_expression.substr(0, colon)
+		var post_colon=new_expression.substr(colon+1)
+		
+		if characters.has(pre_colon):
+			var animation_player=characters[pre_colon].get_node_or_null("AnimationPlayer")
+			if (animation_player!=null):
+				animation_player.play(post_colon)
